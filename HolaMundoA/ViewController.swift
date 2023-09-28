2df0c39c171aa3bb5291353e68c26d720b9fdb61
seriocatalanmunoz <sergio.catalan.munoz@cieep.com>
2023-09-28 13:12:11 +0200

    Ejemplo finalizado

--- HolaMundoA/ViewController.swift
+++ HolaMundoA/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbTexto: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func escribeSaludo(_ sender: Any) {
+        lbTexto.text = "HOLA MUNDO"
+    }
+    
 }
 
