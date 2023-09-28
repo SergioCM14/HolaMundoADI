8c52bc89e10ebecb967404c8ac921379bf1af0be
seriocatalanmunoz <sergio.catalan.munoz@cieep.com>
2023-09-28 12:19:10 +0200

    Initial Commit

---
+++ HolaMundoA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolaMundoA
+//
+//  Created by Serio Catalán Muñoz on 28/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
